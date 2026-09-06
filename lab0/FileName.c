f57303bc34f0556a70d1ab00ed8830e1abf1129c
Q1nt1 <2008pavelpiv@gmail.com>
2026-09-06 11:32:43 +0300

    Добавьте файлы проекта.

---
+++ lab0/FileName.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+
+void main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	getchar();
+	puts("          /|                  ");
+	puts("         / |                  ");
+	puts("        /  |                  ");
+	puts("        \\  |                   ");
+	puts("         \\ |                 ");
+	puts("          \\|              ");
+	puts("_____________________      ");
+	puts("\\                   /      ");
+	puts(" \\                 /      ");
+	puts("  \\               /      ");
+	puts("   \\             /       ");
+	puts("    -------------        ");
+}
